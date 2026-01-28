291a5041cb0ccb08289b4c3892b5fdf7ec11558d
aohajr-bit <aohajr@gmail.com>
2026-01-28 10:05:23 -0600

    Add submit and exam result views

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,24 +1,26 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
-    path('registration/', views.registration_request, name='registration'),
-    path('login/', views.login_request, name='login'),
-    path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
+    # Course list + details
+    path('', views.CourseListView.as_view(), name='index'),
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
+
+    # Enrollment
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
-    # <HINT> Create a route for submit view
+    # Exam submit + result
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='exam_result'
+    ),
 
-    # <HINT> Create a route for show_exam_result view
-
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Auth routes (used by the navbar template tags)
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('registration/', views.registration, name='registration'),
+]
